35e38aaf3b1fc8ca2fba9d0a55b7ad73f215e60e
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-30 03:09:20 -0400

    imported RenderList UI component

--- src/apps/todo-list/TodoList.tsx
+++ src/apps/todo-list/TodoList.tsx
@@ -2,22 +2,35 @@ import React from 'react';
 
 import TodoItem from './TodoItem';
 
+import RenderList from '../../components/ui/RenderList';
+import RenderListItem from '../../components/ui/RenderListItem';
+
 interface TodoListProps {
   items: { id: number; task: string; isDone: boolean }[];
   onDelete: (id: number) => void;
 }
 const TodoList: React.FC<TodoListProps> = ({ items, onDelete }) => {
   return (
-    <ul className="flex flex-col gap-2">
+    <RenderList>
       {items.map((item) => (
-        <TodoItem
-          onDelete={onDelete}
-          todo={item.task}
+        <RenderListItem
           key={item.id}
           id={item.id}
+          onDelete={onDelete}
+          item={item.task}
         />
       ))}
-    </ul>
+    </RenderList>
+    // <ul className="flex flex-col gap-2">
+    //   {items.map((item) => (
+    //     <TodoItem
+    //       onDelete={onDelete}
+    //       todo={item.task}
+    //       key={item.id}
+    //       id={item.id}
+    //     />
+    //   ))}
+    // </ul>
   );
 };
 
