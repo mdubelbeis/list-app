1f113e8bd322e6b55f729619a93ad631caf00c2d
mdubelbeis <mason.dubelbeis@gmail.com>
2022-07-01 23:23:54 -0400

    changed size for question mark icon

--- src/components/icons/QuestionMark.tsx
+++ src/components/icons/QuestionMark.tsx
@@ -6,11 +6,11 @@ const QuestionMark: React.FC<QuestionMarkProps> = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-10 w-10 hover:cursor-help"
+      className="h-12 w-12 hover:cursor-help"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      strokeWidth={2}
+      strokeWidth={1}
     >
       <path
         strokeLinecap="round"
